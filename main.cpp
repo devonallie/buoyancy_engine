5258fe03a85f95df69cc6cf7dd2c27decdac9f29
Devon Allie <35121016+devonallie@users.noreply.github.com>
2021-10-11 19:01:15 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,44 +16,47 @@
 #include <stdio.h>
 #include <iostream>
 #include <unistd.h>
+#include <avr/interrupt.h>
 
-#define DEPTH_TOLERANCE 5;
-#define MAINTAIN_DEPTH_HZ 50;
-#define REACH_DEPTH_HZ 100;
-
-using namespace std;
+#define SURFACE 0
+#define MAX_DEPTH 800
 
 bool target_reached = false;
 bool calendar_set = false; // this won't work because the calendar is lost on boot.
 			   // we don't have non-volatile mem anyway so maybe it's fine?
-typedef enum sources{
+typedef enum command_type{
+	UNSET;
 	CALENDAR;
 	USER;
 	NETWORK;
-}Source;
+	LOW_BATTERY
+}Command;
 
-void command_processing(Source input_source);
+Command command_type = UNSET;
+
+void interrupt_init(void);
 void configure_calendar(void);
+//void configure_network(void);
 
 int main(int argc, char** argv){
 	char option;
-	Source input_source;
 
 	while((option = getopt(argc, argv, "cu:n")) != -1){
 		switch(option){
 			case 'c':
-				input_source = CALENDAR;
+				command_type = CALENDAR;
 				configure_calendar();
 			break;
 			case 'u':
-				input_source = USER;
+				command_type = USER;
 			break;
 			case 'n':
-				input_source = NETWORK;
+				command_type = NETWORK;
+				//configure_network();
 			break;
 		}
-		command_processing(input_source)
 	}
+	interrupt_init();
 	return EXIT_SUCCESS;
 }
 
@@ -89,21 +92,40 @@ void configure_calendar(void)
 
 void load_csv(fstream path)
 {
-	//call some other function to digest the CSV into a database
+	int i = 0;
+	while(fscanf(fstream, //https://ideone.com/08zlcB see here for better version
+	{
 	calendar_set = true;
 	return;
 }
 
-void command_processing(Source input_source){
-	int target_depth = atoi(optarg);
-	while(target_reached = false){
-		poll_rate = reach;
-		if(current_depth() < target_depth - DEPTH_TOLERANCE){
-			dive();
-		}else if(current_depth() > target_depth + DEPTH_TOLERANCE){
-			rise();
-		}else
-			target_reached = true;
-	}
-	poll_rate = maintain;
+/*
+ * void configure_network(void){
+ * This can be used to configure network details from a terminal or have I/O piped through a GUI for config
+ * }
+ */
+
+void interrupt_init(void) {
+	//Set timer for polling battery 
+	//DDR#X = POLLING CIRCUIT SWITCH (OUT)
+	//DDR#X = ADC PIN (IN)
+	//Battery ISR NEEDS max priority
+	//
+	//Set timer for tracking the calendar
+	//
+	//Set timer for polling depth sensor
+	//
+	//Set ISR for instrument loss;
+	//
+	//Set TWI interface
+	sei();
 }
+/*
+ * ISR (vector for battery timer) {
+ * 	BATTER_PIN = HIGH;
+ * 	ADCH or ADCL, these have the results of ADC, a few registers need to be configured to set this up on the right pin
+ *	if(BATTER_DIGITAL_VALUE(taken from ADCH/ADCL) < BATTERY_THRESHOLD)
+ *	{
+ *		command_process(SURFACE, LOW_BATTERY);
+ * 	}
+ * }
